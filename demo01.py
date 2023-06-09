0cafaec73399e7b89273e9487b3303d3561edd5b
烦恼与梦想 <2250021625@qq.com>
2023-06-09 19:38:18 +0800

    第二次提交

--- demo01.py
+++ demo01.py
@@ -5,3 +5,12 @@ from (select s.sid, s.sname, b.sscore, b.cname, dense_rank() over (partition by 
                      from score
                               join course c on score.cid = c.cid) b on s.sid = b.sid) as k
 where rk <= 3;
+
+select *
+from (select s.sid, s.sname, b.sscore, dense_rank() over (partition by department order by sscore desc) as rk
+      from student s
+               join (select sid, sscore, cname
+                     from score
+                              join course c on score.cid = c.cid
+                     where c.cid = 2 ) b on s.sid = b.sid where department='cs') as k
+where rk = 1;
